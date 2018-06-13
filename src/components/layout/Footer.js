6d53247a296aec79d8d78e7cdd1c1854b6207bfe
chagonzles <charlene.gonzles@gmail.com>
2018-06-13 14:25:42 +0800

    Install react dependencies, add reactstrap package and add Layout components

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {Container} from 'reactstrap'
+
+class Footer extends Component {
+
+    render() {
+        return(
+            <div className={'footer'}>
+                <Container>
+                    <span className={'text-muted'}>Cha Gonzales | Powered by Oxford Dictionary API</span>
+                </Container>
+            </div>
+        )
+    }
+    
+}
+
+export default Footer
---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import { Navbar, NavbarBrand } from 'reactstrap'
+
+class Header extends Component {
+
+    render() {
+        return(
+            <Navbar color="dark" dark>
+                <NavbarBrand href="/">Dictionary App</NavbarBrand>
+            </Navbar>
+        )
+    }
+    
+}
+
+export default Header
