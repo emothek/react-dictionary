e82c4b3271d9dafd7a3402e4a3f1e50e6ffe11fb
chagonzles <charlene.gonzles@gmail.com>
2018-06-13 20:43:11 +0800

    Save search and results to localstorage to persist data

--- src/actions/dictionaryAction.js
+++ src/actions/dictionaryAction.js
@@ -4,9 +4,25 @@ import * as dictionaryApi from '../apis/dictionaryApi'
 export const findWordSynonyms = (word) => {
     console.log('search for ', word)
     return(dispatch) => {
-        dictionaryApi.getWordSynonyms(word).then((response)=>{
-          dispatch({type: ActionConstants.SEARCH_RESULTS_LOADED, payload: response.data.results})
-        })
+        //get from localstorage when its a persistent data
+        if(localStorage.getItem(word)) {
+          console.log('get from localstorage!!', word)
+          dispatch({
+              type: ActionConstants.SEARCH_RESULTS_LOADED, 
+              payload: JSON.parse(localStorage.getItem(word))
+          })
+        } 
+        //call API when data is not in localstorage
+        else {
+          console.log('call api!!', word)
+          dictionaryApi.getWordSynonyms(word).then((response)=>{
+              localStorage.setItem(word, JSON.stringify(response.data.results))
+              dispatch({
+                type: ActionConstants.SEARCH_RESULTS_LOADED, 
+                payload: response.data.results
+              })
+          })
+        }
     }
 }
 
